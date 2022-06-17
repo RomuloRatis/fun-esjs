180cf6ce811238a16a27a26ae9e1cccf0d9cd4d9
RomuloRatis <entreratoseleoes@gmail.com>
2022-06-17 01:53:28 -0300

    function

---
+++ JS/script.js
@@ -0,0 +1,33 @@
+function um(){
+    console.log("primeira função do exercício");
+}
+
+um();
+
+
+function dois(nome){
+    console.log("O nome recebido é:", nome)
+}
+
+dois("Vainaweb");
+
+function tres(nome, idade, estiloMusical){
+    console.log("Meu nome é:",nome, "Minha idade é:", idade, "Meu estilo musical:", estiloMusical);
+}
+
+tres("Rômulo.", "29 anos.", "Eclético");
+
+
+function quatro(filme, musica){
+    console.log("filme:", filme, "Música:", musica);
+}
+
+quatro("Parasite.", "after hours");
+
+
+function cinco(numero){
+    return numero * 3;
+}
+
+console.log(cinco(23));
+
